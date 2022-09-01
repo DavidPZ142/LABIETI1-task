8e784b8e7461bcbd9e0155e11d39563c5c13fb50
DavidPZ666 <davidpz616@gmail.com>
2022-08-31 21:13:03 -0500

    first commit

---
+++ src/test/java/com/edu/eci/ieti/taskMicroservice/TaskMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.eci.ieti.taskMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
